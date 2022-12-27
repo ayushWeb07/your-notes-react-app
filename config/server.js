3ad798e794dff8fd5eefb5ee2547bdf473f50a10
wpayush07 <100691164+wpayush07@users.noreply.github.com>
2022-12-27 13:17:26 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['y+XqQ8yzbNf8sNtVuz7Ndw==', 'ButyNYK42L8yiDcu44+w+A==', 'N0iORueQ50osD8blTaDM2w==', 'WS53+Ch1ucNzHz/xcR2XiQ=='],
   },
 });
